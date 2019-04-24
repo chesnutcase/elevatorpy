5469203d881e2db052537484c87fcf2100e9bd46
chesnutcase <chester8991@gmail.com>
2019-04-25 04:37:42 +0800

    fix bug in seed creating passengers with same source and destination

--- elevator/examples/seeds/__init__.py
+++ elevator/examples/seeds/__init__.py
@@ -2,47 +2,53 @@ from ... import Passenger
 import random
 
 
-def generate_n_passengers_with_random_destinations(n, *, max_floor=8):
+def generate_n_passengers_with_random_destinations(n, *, source=1, max_floor=8):
     passengers = []
+
+    def generate_destination(source, max):
+        destination = source
+        while destination == source:
+            destination = random.randint(1, max)
+        return destination
     for i in range(n):
-        passengers.append(Passenger(destination=random.randint(1, max_floor)))
+        passengers.append(Passenger(destination=generate_destination(source, max_floor)))
     return passengers
 
 
 def load_nine_am():
     return {
-        1: generate_n_passengers_with_random_destinations(11),
-        2: generate_n_passengers_with_random_destinations(42),
-        3: generate_n_passengers_with_random_destinations(9),
-        4: generate_n_passengers_with_random_destinations(30),
-        5: generate_n_passengers_with_random_destinations(10),
-        6: generate_n_passengers_with_random_destinations(10),
-        7: generate_n_passengers_with_random_destinations(10),
-        7: generate_n_passengers_with_random_destinations(8),
+        1: generate_n_passengers_with_random_destinations(11, source=1),
+        2: generate_n_passengers_with_random_destinations(42, source=2),
+        3: generate_n_passengers_with_random_destinations(9, source=3),
+        4: generate_n_passengers_with_random_destinations(30, source=4),
+        5: generate_n_passengers_with_random_destinations(10, source=5),
+        6: generate_n_passengers_with_random_destinations(10, source=6),
+        7: generate_n_passengers_with_random_destinations(10, source=7),
+        8: generate_n_passengers_with_random_destinations(8, source=8),
     }
 
 
 def load_twelve_pm():
     return {
-        1: generate_n_passengers_with_random_destinations(9),
-        2: generate_n_passengers_with_random_destinations(10),
-        3: generate_n_passengers_with_random_destinations(18),
-        4: generate_n_passengers_with_random_destinations(17),
-        5: generate_n_passengers_with_random_destinations(13),
-        6: generate_n_passengers_with_random_destinations(39),
-        7: generate_n_passengers_with_random_destinations(10),
-        8: generate_n_passengers_with_random_destinations(7)
+        1: generate_n_passengers_with_random_destinations(9, source=1),
+        2: generate_n_passengers_with_random_destinations(10, source=2),
+        3: generate_n_passengers_with_random_destinations(18, source=3),
+        4: generate_n_passengers_with_random_destinations(17, source=4),
+        5: generate_n_passengers_with_random_destinations(13, source=5),
+        6: generate_n_passengers_with_random_destinations(39, source=6),
+        7: generate_n_passengers_with_random_destinations(10, source=7),
+        8: generate_n_passengers_with_random_destinations(7, source=8)
     }
 
 
 def load_five_pm():
     return {
-        1: generate_n_passengers_with_random_destinations(12),
-        2: generate_n_passengers_with_random_destinations(10),
-        3: generate_n_passengers_with_random_destinations(14),
-        4: generate_n_passengers_with_random_destinations(39),
-        5: generate_n_passengers_with_random_destinations(9),
-        6: generate_n_passengers_with_random_destinations(40),
-        7: generate_n_passengers_with_random_destinations(17),
-        8: generate_n_passengers_with_random_destinations(12)
+        1: generate_n_passengers_with_random_destinations(12, source=1),
+        2: generate_n_passengers_with_random_destinations(10, source=2),
+        3: generate_n_passengers_with_random_destinations(14, source=3),
+        4: generate_n_passengers_with_random_destinations(39, source=4),
+        5: generate_n_passengers_with_random_destinations(9, source=5),
+        6: generate_n_passengers_with_random_destinations(40, source=6),
+        7: generate_n_passengers_with_random_destinations(17, source=7),
+        8: generate_n_passengers_with_random_destinations(12, source=8)
     }
